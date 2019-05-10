e963aa1fe23f0fc5afcac9a1d3f792d61591f1f9
etanadya <nadyanurarrosy@gmail.com>
2019-05-10 20:32:40 +0700

    First Commit

---
+++ app/src/main/java/net/devcamp/animewatch/MainActivity.java
@@ -0,0 +1,33 @@
+package net.devcamp.animewatch;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Size;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static android.util.Size.*;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = new TextView(getApplicationContext());
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+        tv.setLayoutParams(lp);
+        tv.setText("Hari Ini");
+        tv.setTextSize(28);
+        tv.setTextColor(Color.parseColor("#FFFFFF"));
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Russo_One.ttf");
+        tv.setTypeface(tf);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(tv);
+    }
+}
